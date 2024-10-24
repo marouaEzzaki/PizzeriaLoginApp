9bbc96ef68d733c1ea9bc2b4d4283773a3df8fac
Maroua_Ezzaki_Kadmiri <marouaezzaki23@gmail.com>
2024-10-24 20:10:26 +0200

    añadido constructor de dao, la cual contiene los usuarios

--- app/src/main/java/com/example/pizzeria_con_login/persistencia/DaoUsuario.java
+++ app/src/main/java/com/example/pizzeria_con_login/persistencia/DaoUsuario.java
@@ -7,13 +7,27 @@ public class DaoUsuario {
     Usuario user1 = new Usuario();
     Usuario user2 = new Usuario();
     Usuario user3 = new Usuario();
+    Usuario usuariosRegistrados[];
+
+    public DaoUsuario(){
+        user1.setNombre("Maroua");
+        user1.setDireccion("Calle 1");
+        user1.setPassword("1234");
+
+        user2.setNombre("Nadia");
+        user2.setDireccion("Calle 2");
+        user2.setPassword("nadiaguapa");
 
 
+        user3.setNombre("usuario");
+        user3.setDireccion("Calle 3 ");
+        user3.setPassword("4321");
 
-    Usuario usuariosRegistrados[] = {user1, user2, user3};
+        usuariosRegistrados[0] = user1;
+        usuariosRegistrados[1] = user2;
+        usuariosRegistrados[2] = user3;
 
-   // private usuariosGuardados
-
+    }
 
     public Usuario[] getUsuariosRegistrados() {
         return usuariosRegistrados;
