d9862068a66f7ee66f143852f214d0b4bd0e9dd9
Maroua_Ezzaki_Kadmiri <marouaezzaki23@gmail.com>
2024-10-27 18:01:04 +0100

    recibir los datos del usuario desde el LoginActivity y mostrarlos por pantalla

--- app/src/main/java/com/example/pizzeria_con_login/PedirPizzaActivity.java
+++ app/src/main/java/com/example/pizzeria_con_login/PedirPizzaActivity.java
@@ -1,6 +1,7 @@
 package com.example.pizzeria_con_login;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,19 +9,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.pizzeria_con_login.modelo.entidad.Usuario;
+
+import org.w3c.dom.Text;
+
 public class PedirPizzaActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedir_pizza);
 
+        Usuario usuario = (Usuario)getIntent().getSerializableExtra(LogInAcivity.K_USUARIO);
+
+        TextView bienvenidaUsername = findViewById(R.id.bienvenidaUsernmae);
+        TextView userDatosEnvio = findViewById(R.id.userDatosEnvio);
+
+        if (usuario!= null){
+            bienvenidaUsername.setText(usuario.getNombre() + "!");
+            userDatosEnvio.setText(usuario.getDireccion() + ".");
+        }
+
 
         //recoger todos los cooso
         //calcular el precio
         //para hacer la vista de resumen de su pedido
-
-
-
     }
 }
