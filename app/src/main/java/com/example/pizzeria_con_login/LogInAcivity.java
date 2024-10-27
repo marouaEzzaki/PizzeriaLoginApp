b9ca57f2f588aefe7e494a179888e3f0f33eb145
Maroua_Ezzaki_Kadmiri <marouaezzaki23@gmail.com>
2024-10-27 17:57:49 +0100

    mandarle la informacion del usuario a PedirPizzaActivity

--- app/src/main/java/com/example/pizzeria_con_login/LogInAcivity.java
+++ app/src/main/java/com/example/pizzeria_con_login/LogInAcivity.java
@@ -11,8 +11,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.pizzeria_con_login.modelo.entidad.Usuario;
 import com.example.pizzeria_con_login.negocio.GestorUsuario;
+import com.example.pizzeria_con_login.persistencia.DaoUsuario;
 
 public class LogInAcivity extends AppCompatActivity {
+
+    public final static String K_USUARIO = "usuario";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,35 +23,46 @@ public class LogInAcivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+
+
         GestorUsuario gu = new GestorUsuario();
+        DaoUsuario dao = new DaoUsuario();
+
 
         EditText editTextUserName = findViewById(R.id.editTxtUser);
         EditText editTextPassword = findViewById(R.id.editTxtPassword);
         Button botonLogin = findViewById(R.id.btnLogIn);
 
-
-
-
         botonLogin.setOnClickListener(v -> {
             String userName = editTextUserName.getText().toString();
             String userPassword = editTextPassword.getText().toString();
 
-            Usuario usuario = new Usuario();
-            usuario.setNombre(userName);
-            usuario.setPassword(userPassword);
+            Usuario usuarioFromTheApp = new Usuario();
+            usuarioFromTheApp.setNombre(userName);
+            usuarioFromTheApp.setPassword(userPassword);
 
-            boolean esValidado = gu.validarUsuario(usuario);
+            boolean esValidado = gu.validarUsuario(usuarioFromTheApp);
 
             if (esValidado){
-                Log.i("LogInActivity", "Se ha validado correctamente el login del usuario: ");
+                Log.i("LogInActivity", "Se ha validado correctamente el login del usuarioFromTheApp: ");
 
                 Intent intent = new Intent(LogInAcivity.this,PedirPizzaActivity.class);
-                startActivity(intent);
+
+
+                //creo que tengo que pasarle los datos del usuarioFromTheApp
+                Usuario userValidated = dao.getUserByName(userName);
+
+                if (userValidated!=null){
+
+                    intent.putExtra(K_USUARIO,userValidated);
+                    startActivity(intent);
+
+                }
+
             }else{
                 Log.i("LogInActivity", "no se ha validado ");
 
             }
-
         });
     }
 }
